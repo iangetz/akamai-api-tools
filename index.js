05fe2b4d5fdef6629ca2aa58fbb3ef530aa6df7b
Ian Getz <iangetz@tumana.com>
2023-03-27 15:38:00 +0000

    v0.6.0 working version for npm publish

--- index.js
+++ index.js
@@ -1,60 +1,47 @@
 const EdgeGrid = require('akamai-edgegrid');
-const logger = require('../logger');
-
-/*
-	Sample request:
-	const res = await apiTools.getReportData({
-		path: '/reporting-api/v1/reports/opresponses-by-time/versions/2/report-data',
-		method: 'POST',
-		accountSwitchKey: account.accountSwitchKey,
-		body: {
-			"objectIds": [`${cpcode}`],
-			"metrics": ["avgResponseTime"]
-		},
-		logger: 'opresponses-by-time (CP code 112233)',
-		dataProperty: 'data',
-		headers: {header1: 'Header 1', header2: 'Header 2'}                           -> Optional
-		startDate: startDateFormatted,                                                -> Optional
-		endDate: endDateFormatted,                                                    -> Optional
-		interval: 'HOUR',                                                             -> Optional
-		groupId: grp_215885,                                                          -> Optional
-		contractId: ctr_V-3UY193L,                                                    -> Optional
-		arrayToObject: false,                                                         -> Optional
-		objectKey: 'cpcode'                                                           -> Required if arrayToObject is true
-	});
-*/
 
 class ApiTools {
 	constructor(dotEdgerc) {
 		this.dotEdgerc = dotEdgerc;
 	}
 
-	// Standard method to return length for either array or object
-	getLength(input) {
-		const realType = Object.prototype.toString
-			.call(input)
-			.slice(8, -1)
-			.toLowerCase();
+	// endDateOffset (int): Days prior to today for end date (e.g. 0 = through midnight today UTC)
+	// daysPrior (int): Number of days prior to endDate to include in report (e.g. 1 day, 5 days, etc.)
+	// returnDateObj (Boolean): Include endDate Date object in response array
+	getDatesRelative(endDateOffset = 0, daysPrior = 1, returnEndDateObj = false) {
+		// New date 'endDateOffset' days ago
+		const endDate = new Date();
+		endDate.setUTCDate(endDate.getUTCDate() - endDateOffset);
+		endDate.setUTCHours(0,0,0,0);
 
-		switch(realType) {
-			case 'array':
-				return input.length;
-			case 'object':
-				return Object.keys(input).length;
-			default:
-				return undefined;
+		// End date formatted for Akamai {OPEN} API
+		const endDateFormatted = `${endDate.toISOString().split('.')[0]}Z`;
+
+		// Start date formatted for Akamai {OPEN} API
+		let startDate = new Date();
+		startDate.setUTCDate(startDate.getUTCDate() - (endDateOffset + daysPrior));
+		startDate.setUTCHours(0,0,0,0);
+		const startDateFormatted = `${startDate.toISOString().split('.')[0]}Z`;
+
+		if(returnEndDateObj) {
+			return [startDateFormatted, endDateFormatted, endDate];
+		} else {
+			return [startDateFormatted, endDateFormatted];
 		}
 	}
 
-	getReportDates(weeksBack = 0, duration = 1) {
+	// Custom use case: Report is run each week between Monday - Thursday morning for the prior week ending last Friday (a buffer since data for prior two days is not final)
+	// weeksBack (int): Weeks prior to last week (0 = last week ending Friday)
+	// weeks (int): Number of weeks prior to endDate to include in report
+	// returnDateObj (Boolean): Include endDate Date object in response array
+	getDatesToFriday(weeksBack = 0, weeks = 1, returnEndDateObj = false) {
 		let endDate = new Date();
 
-		// Control history offset for initial Google Sheets setup (see config file)
+		// Used if populating a new dashboard to capture weekly data from <weeksback> weeks ago
 		if(weeksBack > 0) {
 			endDate.setUTCDate(endDate.getUTCDate() - (weeksBack * 7));
 		}
 
-		// Report is run each Monday morning for the prior week ending last Friday (reporting data for prior two days is not final)
 		// Move to prior week if current date is not between Mon - Thu
 		if(endDate.getUTCDay() < 1 || endDate.getUTCDay() > 4) {
 			endDate.setUTCDate(endDate.getUTCDate() - 7);
@@ -68,53 +55,49 @@ class ApiTools {
 
 		// startDate
 		let startDate = new Date(endDate);
-		startDate.setUTCDate(startDate.getUTCDate() - (7 * duration));
+		startDate.setUTCDate(startDate.getUTCDate() - (7 * weeks));
 		startDate.setUTCHours(0,0,0,0);
 		const startDateFormatted = `${startDate.toISOString().split('.')[0]}Z`;
 
-		return [startDateFormatted, endDateFormatted, endDate];
+		if(returnEndDateObj) {
+			return [startDateFormatted, endDateFormatted, endDate];
+		} else {
+			return [startDateFormatted, endDateFormatted];
+		}
 	}
 
-	// endDateStr (string): Date value formatted as "YYYY-MM-DD")
-	// duration (integer): Number of weeks prior to endDate
-	getOffcycleDates(endDateStr, duration = 2) {
-		const [year, month, date] = endDateStr.split("-")
-		const endDate = new Date(year, month-1, date);
-
-		// End date formatted for Akamai {OPEN} API
-		const endDateFormatted = `${endDate.toISOString().split('.')[0]}Z`;
-
-		// Start date formatted for Akamai {OPEN} API
-		let startDate = new Date(year, month-1, date);
-		startDate.setUTCDate(startDate.getUTCDate() - (7 * duration));
-		const startDateFormatted = `${startDate.toISOString().split('.')[0]}Z`;
-
-		return [startDateFormatted, endDateFormatted, endDate];
-	}
-
+	// accountSwitchKey (string): Send empty '' value if not known
+	// startDateFormatted (string): Formatted Akamai API timestamp for start of reporting period
+	// endDateFormatted (string): Formatted Akamai API timestamp for end of reporting period
 	// count (int): number of CP codes to return in array
 	// measurement (string): hits|bytes
 	// level (string): edge|origin
-	// products [array of strings]: ['Ion Standard'] == 
+	// products [array]: Limit reporting data to specific products e.g. ['Ion Standard']
 	async getTopCpcodes(accountSwitchKey, startDateFormatted, endDateFormatted, count=10, measurement='hits', level='edge', products=[]) {
 		// Determine what API to call
-		let path, metric, logger;
+		let path, metric;
 		if(measurement === 'hits') {
 			path = '/reporting-api/v1/reports/hits-by-cpcode/versions/1/report-data';
 			metric = level === 'edge' ? 'edgeHits' : 'originHits';
-			logger = 'Top hits-by-cpcode';
 		}
 		else if(measurement === 'bytes') {
 			path = '/reporting-api/v1/reports/bytes-by-cpcode/versions/1/report-data';
 			metric = level === 'edge' ? 'edgeBytes' : 'originBytes';
-			logger = 'Top bytes-by-cpcode';
 		} else {
 			// Not an acceptable measurement value
-			return [];
+			return [
+				{
+					responseCode: 400,
+					error: 'Bad Request: Invalid measurement value',
+					length: 0,
+					timer: 0
+				},
+				[]
+			];
 		}
 
 		// Get CP code data for either hits or bytes
-		const byCpcode = await this.getReportData({
+		const [logHitByte, byCpcode] = await this.getReportData({
 			path: path,
 			method: 'POST',
 			accountSwitchKey: accountSwitchKey,
@@ -125,52 +108,134 @@ class ApiTools {
 				"objectIds": "all",
 				"metrics": [metric]
 			},
-			logger: logger,
 			dataProperty: 'data',
 		});
 
-		// Filter by product, if specified in params
-		let productCpcodes;
-		if(products.length > 0) {
-			const cpcodeDetails = await this.getReportData({
-				path: '/cprg/v1/cpcodes',
-				method: 'GET',
-				accountSwitchKey: accountSwitchKey,
-				body: {},
-				logger: 'CP code details',
-				dataProperty: 'cpcodes',
-				arrayToObject: true,
-				objectKey: 'cpcodeId'
-			});
+		if(logHitByte.responseCode === 200) {
+			// Filter by product, if specified in params
+			let productCpcodes, logCp, cpcodeDetails;
+			if(products.length > 0) {
+				[logCp, cpcodeDetails] = await this.getReportData({
+					path: '/cprg/v1/cpcodes',
+					method: 'GET',
+					accountSwitchKey: accountSwitchKey,
+					body: {},
+					dataProperty: 'cpcodes',
+					arrayToObject: true,
+					objectKey: 'cpcodeId'
+				});
 
-			productCpcodes = byCpcode.filter(elem => {
-				if(cpcodeDetails[elem.cpcode].products.length > 0 && products.includes(cpcodeDetails[elem.cpcode].products[0].productName)) {
-					return true;
-				}
-			});
-		} else {
-			// No filter, include all CP codes
-			productCpcodes = [...byCpcode];
-		}
-
-		let topCpcodes = [];
-		if(productCpcodes.length > 0) {
-			// Sort descending order, largest first
-			productCpcodes.sort((a,b) => {
-				return b[metric] - a[metric];
-			});
-
-			// Trim list
-			if(productCpcodes.length > count) {
-				productCpcodes.length = count;
+				productCpcodes = byCpcode.filter(elem => {
+					if(cpcodeDetails[elem.cpcode].products.length > 0 && products.includes(cpcodeDetails[elem.cpcode].products[0].productName)) {
+						return true;
+					}
+				});
+			} else {
+				// No filter, include all CP codes
+				productCpcodes = [...byCpcode];
 			}
 
-			topCpcodes = productCpcodes.map(elem => elem.cpcode);
-		}
+			let topCpcodes = [];
+			if(productCpcodes.length > 0) {
+				// Sort descending order, largest first
+				productCpcodes.sort((a,b) => {
+					return b[metric] - a[metric];
+				});
 
-		return topCpcodes;
+				// Trim list
+				if(productCpcodes.length > count) {
+					productCpcodes.length = count;
+				}
+
+				topCpcodes = productCpcodes.map(elem => elem.cpcode);
+			}
+
+			if(typeof logCp === 'undefined') {
+				return [logHitByte, topCpcodes];
+			} else {
+				logCp.timer = logHitByte.timer + logCp.timer;
+				return [logCp, topCpcodes];
+			}
+
+		} else {
+			return [logHitByte, []];
+		}
 	}
 
+	/*
+		Get CP code data for either hits or bytes
+
+		Sample
+		const res = await apiTools.getReportData({
+			path: '/reporting-api/v1/reports/opresponses-by-time/versions/2/report-data',
+			method: 'POST',
+			accountSwitchKey: account.accountSwitchKey,				-> Ignore if not known (for Akamai employees)
+			body: {													-> Body varies by API requirements
+				"objectIds": ["123456"],
+				"metrics": ["avgResponseTime"]
+			},
+			dataProperty: "data",
+			params: "depth=ALL"										-> Optional
+			startDate: startDateFormatted,							-> Optional
+			endDate: endDateFormatted,								-> Optional
+			interval: "HOUR",										-> Optional
+			groupId: "grp_123456",									-> Optional
+			contractId: "ctr_A-B0C1D2E",							-> Optional
+			headers: {header1: "Header 1", header2: "Header 2"}		-> Optional
+			arrayToObject: false,									-> Optional
+			objectKey: "cpcode"										-> Required if arrayToObject is true
+		});
+	*/
+	async getReportData(reqObj) {
+		// Build URI used to make the API call
+		let uri = `${reqObj.path}`;
+		if('accountSwitchKey' in reqObj) {
+			uri += `?accountSwitchKey=${reqObj.accountSwitchKey}`
+		} else {
+			uri += '?accountSwitchKey=';
+		}
+		if('params' in reqObj) {uri += `&params=${reqObj.params}`};
+		if('startDate' in reqObj) {uri += `&start=${encodeURIComponent(reqObj.startDate)}`;}
+		if('endDate' in reqObj) {uri += `&end=${encodeURIComponent(reqObj.endDate)}`;}
+		if('interval' in reqObj) {uri += `&interval=${reqObj.interval}`;}
+		if('groupId' in reqObj) {uri += `&groupId=${reqObj.groupId}`;}
+		if('contractId' in reqObj) {uri += `&contractId=${reqObj.contractId}`;}
+		reqObj.uri = uri;
+
+		// Headers
+		reqObj.fullHeaders = {"Content-Type": "application/json"};
+		if('headers' in reqObj) {Object.assign(reqObj.fullHeaders, reqObj.headers)}
+
+		// The actual call to the Akamai {OPEN} API with logic if response status code is not 200
+		let attempts = 0, retries = 10, log = {}, data;
+		while(attempts < retries && log.responseCode !== 200) {
+			attempts++;
+			console.log(`Attempt #${attempts} for ${reqObj.path}`);
+
+			// Make API call and capture response time
+			const timerStart = new Date();
+			[log, data] = await this.makeApiCall(reqObj);
+			const timerStop = new Date();
+			log.timer = Math.round(Math.abs(timerStop - timerStart));
+
+			if(log.responseCode !== 200) {
+				// If response was not 200, then 'data' variable was never assigned and value is 'undefined'
+				console.log(`No luck (HTTP ${log.responseCode})`);
+
+				if(attempts === retries) {
+					log.error = (`${retries} unsuccessful attempts to Akamai {OPEN} API`);
+					return [log, data];
+				}
+
+				// Wait for 5 seconds before starting next retry
+				await new Promise(r => setTimeout(r, 5000));
+			}
+		}
+
+		return [log, data];
+	}
+
+	// Internal function, called by getReportData(), that actually makes the API call
 	async makeApiCall(reqObj) {
 		// Initiate Akamai Edgerc
 		const edgerc = new EdgeGrid({
@@ -188,13 +253,18 @@ class ApiTools {
 			}).send((error, response, body) => {
 				let log = {}, data;
 				if(typeof response != 'undefined' && response.status === 200) {
-					// Get data from appropriate response property (varies by {OPEN} API)
-					const res = reqObj.dataProperty
-					.split('.')
-					.reduce((obj, item) => {
-						return obj && obj[item];
-					}, JSON.parse(body));
-					
+					// Get data from appropriate response property (varies by {OPEN} API), if specified
+					let res;
+					if('dataProperty' in reqObj) {
+						res = reqObj.dataProperty
+							.split('.')
+							.reduce((obj, item) => {
+								return obj && obj[item];
+							}, JSON.parse(body));
+					} else {
+						res = JSON.parse(body);
+					}
+
 					// Log stats
 					log.responseCode = response.status;
 					log.length = this.getLength(res);
@@ -219,48 +289,55 @@ class ApiTools {
 		});
 	}
 
-	async getReportData(reqObj) {
-		// Build the actual URI used to make the API call based on provided values
-		let uri = `${reqObj.path}?accountSwitchKey=${reqObj.accountSwitchKey}`;
-		if('startDate' in reqObj) {uri += `&start=${encodeURIComponent(reqObj.startDate)}`;}
-		if('endDate' in reqObj) {uri += `&end=${encodeURIComponent(reqObj.endDate)}`;}
-		if('interval' in reqObj) {uri += `&interval=${reqObj.interval}`;}
-		if('groupId' in reqObj) {uri += `&groupId=${reqObj.groupId}`;}
-		if('contractId' in reqObj) {uri += `&contractId=${reqObj.contractId}`;}
-		reqObj.uri = uri;
+	// Convert product ID to a human-friendly product name
+	getProductName(productId) {
+		const papiProductKeys = {
+			API_Accel: 'API Acceleration',
+			Adaptive_Media_Delivery: 'Adaptive Media Delivery',
+			Alta: 'Terra Alta Enterprise Accelerator',
+			Aqua_Mobile: 'Aqua Mobile',
+			DCP: 'IoT Edge Connect',
+			Download_Delivery: 'Download Delivery',
+			Dynamic_Site_Del: 'Dynamic Site Delivery',
+			EdgeConnect: 'Cloud Monitor Data Delivery',
+			Edge_Connect_Message_Store: 'Edge Connect Message Store',
+			Fresca: 'Ion Standard',
+			HTTP_Content_Del: 'HTTP Content Delivery',
+			HTTP_Downloads: 'HTTP Downloads',
+			IoT: 'IoT',
+			Obj_Caching: 'Object Caching',
+			Obj_Delivery: 'Object Delivery',
+			Progressive_Media: 'Progressive Media Downloads',
+			RM: 'Ion Media Advanced',
+			Rich_Media_Accel: 'Rich Media Accelerator',
+			SPM: 'Ion Premier',
+			Security_Failover: 'Cloud Security Failover',
+			Site_Accel: 'Dynamic Site Accelerator',
+			Site_Defender: 'Kona Site Defender',
+			Site_Del: 'Dynamic Site Delivery Legacy',
+			WebAP: 'Web Application Protector',
+			Web_App_Accel: 'Web Application Accelerator'
+		};
 
-		// Headers
-		reqObj.fullHeaders = {"Content-Type": "application/json"};
-		if('headers' in reqObj) {Object.assign(reqObj.fullHeaders, reqObj.headers)}
+		return papiProductKeys[productId];
+	}
 
-		// The actual call to the Akamai {OPEN} API with logic if response status code is not 200
-		let attempts = 0, retries = 10, log = {}, data;
-		while(attempts < retries && log.responseCode !== 200) {
-			attempts++;
-			console.log(`Attempt #${attempts} for ${reqObj.path}`);
-			// If response was not 200, then 'data' variable was never assigned and value is 'undefined'
-			[log, data] = await this.makeApiCall(reqObj);
+	// Internal standard method to return length for either array or object
+	getLength(input) {
+		const realType = Object.prototype.toString
+			.call(input)
+			.slice(8, -1)
+			.toLowerCase();
 
-			if(log.responseCode !== 200) {
-				console.log(`No luck (HTTP ${responseCode})`);
-
-				if(attempts === retries) {
-					log.err = (`${retries} attempts to Akamai {OPEN} API`);
-					return [log, data];
-				}
-
-				// Despite not getting an HTTP 200 response, delete all object data in case something was assigned
-				for (let member in data) delete data[member];
-				// One day I'll delete these safety lines
-				console.debug('Safety measure...');
-				console.debug({data});
-
-				// Wait for 5 seconds before starting next retry
-				await new Promise(r => setTimeout(r, 5000));
-			}
+		switch(realType) {
+			case 'string':
+			case 'array':
+				return input.length;
+			case 'object':
+				return Object.keys(input).length;
+			default:
+				return undefined;
 		}
-
-		return [log, data];
 	}
 }
 
